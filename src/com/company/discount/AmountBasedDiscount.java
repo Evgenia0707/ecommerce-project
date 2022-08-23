98ae57345c8db269683b4398b4476b85ead76cb5
Evgenia0707 <4030071@gmail.com>
2022-08-23 16:17:24 -0400

    Ecommerce project

---
+++ src/com/company/discount/AmountBasedDiscount.java
@@ -0,0 +1,21 @@
+package com.company.discount;
+
+import java.util.UUID;
+
+public class AmountBasedDiscount extends Discount {
+    private Double discountAmount;
+    public AmountBasedDiscount(UUID id, String name, Double thresholdAmount, Double discountAmount) {
+        super(id, name, thresholdAmount);
+        this.discountAmount = discountAmount;
+    }
+    public AmountBasedDiscount(UUID id, String name, Double thresholdAmount) {
+        super(id, name, thresholdAmount);
+    }
+    @Override
+    public Double calculateCartAmountAfterDiscountApplied(Double amount) {
+        return amount - discountAmount;
+    }
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
+}
